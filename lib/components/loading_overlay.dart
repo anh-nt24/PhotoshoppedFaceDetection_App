c3dd5ed4d6c57839e0ea3f14a99047c1d31b66e7
= <theanh24013@gmail.com>
2024-06-23 11:55:20 +0700

    add loading effect

---
+++ lib/components/loading_overlay.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class LoadingOverlay extends StatelessWidget {
+	final bool isLoading;
+	final Widget child;
+
+	const LoadingOverlay({
+		Key? key,
+		required this.isLoading,
+		required this.child,
+	}) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) {
+		return Stack(
+			children: [
+				child,
+				if (isLoading)
+					Container(
+						color: Colors.black.withOpacity(0.8),
+						child: Center(
+							child: LoadingAnimationWidget.flickr(
+								leftDotColor: Color.fromRGBO(221, 102, 99, 1.0), 
+								rightDotColor: Color.fromRGBO(252, 242, 237, 1.0), 
+								size: 50
+							),
+						),
+					),
+			],
+		);
+	}
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import '../constants.dart';
 
 class ApiService {
-  	static const String baseUrl = 'http://192.168.1.8:8000/api';
 
 	Future<http.StreamedResponse?> analyzeImage(http.MultipartFile imageFile, BuildContext context) async {
 		try {
-			String url = '${baseUrl}/detect-regions/';
+			String url = '${Constants.baseUrl}/api/detect-regions/';
 
 			var request = http.MultipartRequest('POST', Uri.parse(url));
 			request.headers.addAll({
